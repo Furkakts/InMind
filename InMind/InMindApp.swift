aaa8f2ecb9e5fa571d2a2980c72d0b53b3c9c279
Furkan Aktaş <furkakts_4126@hotmail.com>
2024-07-19 00:37:14 +0300

    Initial Commit

---
+++ InMind/InMindApp.swift
@@ -0,0 +1,17 @@
+//
+//  InMindApp.swift
+//  InMind
+//
+//  Created by Furkan on 19.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct InMindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
