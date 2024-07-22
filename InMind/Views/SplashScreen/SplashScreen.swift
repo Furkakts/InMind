8f3079dfcdd52928bee06acc6a4d9fdb121b6898
Furkan Aktaş <furkakts_4126@hotmail.com>
2024-07-23 00:14:21 +0300

    [Update] on Appearance of SplashScreen
    Values of corner radius and height of error message are rearranged/decreased.

--- InMind/Views/SplashScreen/SplashScreen.swift
+++ InMind/Views/SplashScreen/SplashScreen.swift
@@ -11,39 +11,45 @@ struct SplashScreen: View {
                 .font(.system(size: 50, weight:.semibold, design: .rounded))
                 .foregroundStyle(Color("SecondaryColor"))
             Spacer()
-            VStack(spacing:50){
-                if cdModel.isErrorOccurred {
-                    Text("Unknown error occurred.Please try again!")
-                        .padding()
-                        .font(.subheadline)
-                        .frame(maxWidth:.infinity)
-                        .frame(height:50)
-                        .foregroundStyle(Color("MainColor"))
-                        .background(Color("SecondaryColor"), in:RoundedRectangle(cornerRadius: 5))
-                } else {
-                    ProgressView()
-                    .font(.title)
-                    .controlSize(.regular)
-                    .tint(Color("SecondaryColor"))
-                }
-                    
-            Text("\(Bundle.main.appVersionLong).\(Bundle.main.appBuild)")
-                    .font(.footnote)
-                    .fontWeight(.semibold)
-                    .foregroundStyle(Color("SecondaryColor"))
-            }
+            bottomSection
+                .padding(.horizontal)
         }
         .onAppear{
             cdModel.setContainer()
         }
     }
+    
+    // Shows Version and Loading Sign or Error Text based on case while loading data from local database.
+    var bottomSection: some View {
+        VStack(spacing:50){
+            if cdModel.isErrorOccurred {
+                Text("Unknown error occurred.Please Try again!")
+                    .padding()
+                    .font(.subheadline)
+                    .frame(maxWidth:.infinity)
+                    .frame(height:25)
+                    .foregroundStyle(Color("MainColor"))
+                    .background(Color("SecondaryColor"), in:RoundedRectangle(cornerRadius: 3))
+            } else {
+                ProgressView()
+                    .font(.title)
+                    .controlSize(.regular)
+                    .tint(Color("SecondaryColor"))
+            }
+                
+            Text("\(Bundle.main.appVersionLong).\(Bundle.main.appBuild)")
+               .font(.footnote)
+               .fontWeight(.semibold)
+               .foregroundStyle(Color("SecondaryColor"))
+        }
+    }
 }
-
+  // Gets application's version and build
 extension Bundle {
     public var appBuild: String          { getInfo("CFBundleVersion") }
     public var appVersionLong: String    { getInfo("CFBundleShortVersionString") }
     
-    fileprivate func getInfo(_ str: String) -> String { infoDictionary?[str] as? String ?? "" }
+    fileprivate func getInfo(_ str: String) -> String { infoDictionary?[str] as? String ?? "?"}
 }
 
 struct SplashScreen_Previews: PreviewProvider {
